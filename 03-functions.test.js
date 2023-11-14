902ceb5af9921dd37f0a7be99791bc36a6c5d9b8
kontroldev <raulgallego79@icloud.com>
2023-11-14 19:31:57 +0100

    Ultimo commit

--- 03-functions.test.js
+++ 03-functions.test.js
@@ -8,8 +8,9 @@ describe("Funciones en JavaScript", () => {
     it("Las funciones pueden tener 1 parámetro", () => {
         let name = "Rosa";
 
-        // Escribe aquí tu funcion "saludar"
-
+        function saludar(nombre) {
+            return "Hola, " + nombre;
+        }
 
         assert.strictEqual(saludar(name),"Hola, Rosa");
     })
@@ -19,8 +20,9 @@ describe("Funciones en JavaScript", () => {
         let num1 = 17;
         let num2 = 10;
 
-        // Escribe aquí tu función "suma"
-
+        function suma(a, b) {
+            return a + b;
+        }
 
         assert.strictEqual(suma(num1, num2),27);
     })
@@ -33,27 +35,29 @@ describe("Funciones en JavaScript", () => {
         let weather = "soleado";
         let rainProbability = "10%";
 
-        // Escribe aquí tu función "weatherReport" (fíjate en la aserción para saber qué debe retornar esta función).
+        function weatherReport(userName, name, city, weather, rainProbability) {
+            return `Hola ${name} (${userName}), hoy en ${city} el tiempo es ${weather} y las probabilidades de lluvia son del ${rainProbability}.`;
+        }
 
-        assert.strictEqual(weatherReport(userName, name, city, weather, rainProbability)).toEqual("Hola Ada Martín (ada89), hoy en Las Vegas el tiempo es soleado y las probabilidades de lluvia son del 10%.");
-    })
+        assert.strictEqual(
+            weatherReport(userName, name, city, weather, rainProbability),
+            "Hola Ada Martín (ada89), hoy en Las Vegas el tiempo es soleado y las probabilidades de lluvia son del 10%."
+    });
 
+    
 
 
     // BONUS: Crea un test que te permita testear la función que verás a continuación. Para ello, también te hemos proporcionado las variables que necesitarás (y alguna más que no necesitarás, para confundir :P).
     // Recuerda quitar la puntuación de comentario (/* ... */) para que este código a continuación sea legible, e inclúyelo dentro de tu test.
 
 
-     /*    let userName = "Ariana28";
-        let userAge = 38;
-        let userEmail = "ariana_28@gmail.com";
-        let userLocation = "Barcelona"
-        let userIsRegistered = true;
-        let password = "p4s$w0rDs3guR0";
-
-        function userAuth(userName, userEmail, password) {
-            return "Welcome ${userName}, your email is ${userEmail} and your password is ${password}."
-        } */
-
-
-    })
+    /*let userName = "Ariana28";
+    let userAge = 38;
+    let userEmail = "ariana_28@gmail.com";
+    let userLocation = "Barcelona";
+    let userIsRegistered = true;
+    let password = "p4s$w0rDs3guR0";
+    
+    function userAuth(userName, userEmail, password) {
+        return `Welcome ${userName}, your email is ${userEmail} and your password is ${password}.`;
+    }*/
